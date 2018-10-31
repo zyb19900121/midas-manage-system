2e8935c6891fc3ca1758bff50dc3eda9a563a2bd
zhangyanbin <zhangyanbin@300.cn>
2018-10-31 21:04:14 +0800

    1.搭建项目（UI部分）

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,14 +1,65 @@
 import React, { Component } from "react";
+import { Menu, Icon } from "antd";
+import "assets/css/app.less";
+
+const SubMenu = Menu.SubMenu;
+
 class Aside extends Component {
+  state = {
+    collapsed: false
+  };
+
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  };
+
   render() {
     return (
       <div>
-        {" "}
-        <h1>This is a component!</h1>{" "}
+        <div className="logo">Midas</div>
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          <Menu.Item key="1">
+            <Icon type="pie-chart" />
+            <span>Coming Soon</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Icon type="desktop" />
+            <span>Coming Soon</span>
+          </Menu.Item>
+          <SubMenu
+            key="sub1"
+            title={
+              <span>
+                <Icon type="user" />
+                <span>Coming Soon</span>
+              </span>
+            }
+          >
+            <Menu.Item key="3">Coming Soon</Menu.Item>
+            <Menu.Item key="4">Coming Soon</Menu.Item>
+            <Menu.Item key="5">Coming Soon</Menu.Item>
+          </SubMenu>
+          <SubMenu
+            key="sub2"
+            title={
+              <span>
+                <Icon type="team" />
+                <span>Coming Soon</span>
+              </span>
+            }
+          >
+            <Menu.Item key="6">Coming Soon</Menu.Item>
+            <Menu.Item key="8">Coming Soon</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9">
+            <Icon type="file" />
+            <span>Coming Soon</span>
+          </Menu.Item>
+        </Menu>
       </div>
     );
   }
 }
 export default Aside;
-
-
--- src/views/app.js
+++ src/views/app.js
@@ -1,23 +1,45 @@
 import React, { Component } from "react";
+import { Layout, Breadcrumb } from "antd";
+import Aside from "components/Aside";
 import "assets/css/app.less";
-import { Layout } from "antd";
-
-import Aside from 'components/Aside'
 
 const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
+  state = {
+    collapsed: false
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+
   render() {
     return (
       <div className="app">
-        <Layout>
-          <Sider>
-						<Aside></Aside>
-					</Sider>
+        <Layout style={{ minHeight: "100vh" }}>
+          <Sider
+            collapsible
+            collapsed={this.state.collapsed}
+            onCollapse={this.onCollapse}
+          >
+            <Aside />
+          </Sider>
           <Layout>
-            <Header>Header</Header>
-            <Content>Content</Content>
-            <Footer>Footer</Footer>
+            <Header style={{ background: "#fff", padding: 0 }} />
+            <Content style={{ margin: "0 16px" }}>
+              <Breadcrumb style={{ margin: "16px 0" }}>
+                <Breadcrumb.Item>商品</Breadcrumb.Item>
+                <Breadcrumb.Item>添加商品</Breadcrumb.Item>
+              </Breadcrumb>
+              <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
+                Content
+              </div>
+            </Content>
+            <Footer style={{ textAlign: "center" }}>
+              Midas ©2018 Created by Midas
+            </Footer>
           </Layout>
         </Layout>
       </div>
